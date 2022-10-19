812b41bce26284e968a4eb621c687fcc0c0ad81b
Priyanka Shukla <priyankashukla769@gmail.com>
2022-10-19 22:39:30 +0530

    chore: add constant file

---
+++ dags/constant.py
@@ -0,0 +1,7 @@
+DATABASE_ENGINE = "postgresql+psycopg2://postgres:<password>@localhost:5432/<database name>" #insert database engine url here
+SPOTIFY_USER_ID = "" #insert Spotify user id here
+TOKEN_URL = 'https://accounts.spotify.com/api/token'
+BASE_URL = 'https://api.spotify.com/v1/'
+CLIENT_ID = "" #insert client id here
+CLIENT_SECRET = "" #insert client secret here
+CHROME_DRIVER_PATH = "/usr/local/bin/chromedriver"
